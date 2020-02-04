af2c3b25e523df5b0d5503cc19592ba0b74a95bc
Mark Fisher <mfishe@gmail.com>
2020-02-04 15:02:57 +0000

    Create local development server using express
    Run using `npm run serve`

---
+++ scripts/serve.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const fs = require('fs');
+const morgan = require('morgan');
+const defaultHeaders = require('./server/default_headers');
+
+const PORT = 8888;
+
+const app = express();
+
+// Logging middleware
+app.use(morgan('dev'));
+
+// Everything is static!
+app.use(express.static('public', {
+    index: 'index',
+    setHeaders: function (res, path, stat) {
+        // Just need to set Content-Type for requests without an extension
+        if(path.split('/').pop().indexOf('.') === -1 ) {
+
+            // Set default headers for documents only
+            res.set('Content-Type', 'text/html; charset=utf-8');
+            defaultHeaders.document.forEach(header => {
+                res.set(header.name, header.value);
+            }) ;
+        }
+
+        // Set default headers
+        defaultHeaders.all.forEach(header => {
+            res.set(header.name, header.value);
+        });
+    }
+}));
+
+// static middleware only calls next() if it couldn't find the file
+app.use((req, res, next) =>{
+    res.status(404);
+    fs.createReadStream("public/404").pipe(res)
+});
+
+
+// Start server
+app.listen(PORT, () => {
+   console.log(`Server is listening on ${PORT}`);
+});
+
+// Open in default browser
+require('opn')(`http://localhost:${PORT}`);
---
+++ scripts/server/default_headers.js
@@ -0,0 +1,27 @@
+// HTTP response headers that should be set for all, or nearly all, requests
+
+module.exports =  {
+    'all': [
+        // Response headers to be sent for all Content-Types
+        {
+            name: 'X-Content-Type-Options',
+            value: 'nosniff'
+        }
+    ],
+    'document': [
+        // Response headers to be sent only if the Content-Type is text/html
+        {
+            name: 'Content-Security-Policy',
+            value: "font-src 'self'; base-uri 'self'; connect-src 'self'; form-action 'self'; frame-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; object-src 'none'; frame-ancestors 'self'; img-src 'self';"
+        },
+        {
+            name: 'Feature-Policy',
+            value: "camera 'none'; microphone 'none'; geolocation 'none'; autoplay 'none'; speaker 'none'; fullscreen 'none';  geolocation 'none'; sync-xhr 'none'; midi 'none'; usb 'none'"
+
+        },
+        {
+            name: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin'
+        },
+    ]
+};
