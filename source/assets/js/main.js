3648ff679a48ea17964b29098fbe54f075fda0f6
Mark Fisher <mfishe@gmail.com>
2020-08-02 14:14:57 +0100

    Remove Busgnag

--- source/assets/js/main.js
+++ source/assets/js/main.js
@@ -1,6 +1,4 @@
 import Vue from "vue";
-import Bugsnag from "@bugsnag/js";
-import BugsnagPluginVue from "@bugsnag/plugin-vue";
 import { VueMasonryPlugin } from "vue-masonry";
 import explore from "./pages/explore.vue";
 import contact from "./pages/contact.vue";
@@ -12,13 +10,6 @@ require("../css/main.scss");
 // Note dynamic imports currently only used in IE11 (fetch polyfill)
 // eslint-disable-next-line camelcase,no-undef
 __webpack_public_path__ = "assets/";
-
-Bugsnag.start({
-    apiKey: "b1a1880b2f0b2119db9d3d82e2ac2e50",
-    plugins: [
-        new BugsnagPluginVue(),
-    ],
-});
 
 if (document.getElementById("explore-app")) {
     Vue.use(VueMasonryPlugin);
