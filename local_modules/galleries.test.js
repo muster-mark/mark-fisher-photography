98dc60a2b12c566dd311f7a2eef771f77656af49
Mark Fisher <mfishe@gmail.com>
2020-08-13 20:04:07 +0100

    Update npm packages

--- local_modules/galleries.test.js
+++ local_modules/galleries.test.js
@@ -1,5 +1,5 @@
-const galleries = require('./galleries.js');
+const galleries = require("./galleries.js");
 
-test('Meta descriptions all end in full stop', () => expect(Object.values(galleries.metaDescriptions)
+test("Meta descriptions all end in full stop", () => expect(Object.values(galleries.metaDescriptions)
     .map(description => description[description.length - 1])
-    .every(last => last === '.')).toBeTruthy());
+    .every(last => [".", "?", "!"].indexOf(last) !== -1)).toBeTruthy());
