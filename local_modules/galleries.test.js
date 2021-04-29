139fb7dbd4236d11213ee466d16413fe19d22329
Mark Fisher <mfishe@gmail.com>
2021-04-29 12:59:51 +0100

    Fix false failure of test galleries.test.js

--- local_modules/galleries.test.js
+++ local_modules/galleries.test.js
@@ -1,4 +1,4 @@
-const { galleries } = require("./galleries.json").galleries;
+const { galleries } = require("./galleries.json");
 
 test("Meta descriptions all end in full stop", () => expect(galleries
     .map(gallery => gallery.metaDescription[gallery.metaDescription.length - 1])
