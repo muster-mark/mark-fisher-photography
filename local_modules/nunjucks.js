7c5e4dc55e203b3c3cfd70ed7bc8a52efd8f8230
Mark Fisher <mfishe@gmail.com>
2021-12-23 16:14:48 +0000

    Add structured data for photographsx

--- local_modules/nunjucks.js
+++ local_modules/nunjucks.js
@@ -41,5 +41,20 @@ environment.addFilter("captionToMetaDesc", caption => {
     }
     return desc;
 });
+/**
+ * Returns a JSON stringified version of the value, safe for inclusion in an
+ * inline <script> tag. The optional argument 'spaces' can be used for
+ * pretty-printing.
+ *
+ * Output is NOT safe for inclusion in HTML! If that's what you need, use the
+ * built-in 'dump' filter instead.
+ */
+environment.addFilter("json", function (value, spaces) {
+    if (value instanceof nunjucks.runtime.SafeString) {
+        value = value.toString();
+    }
+    const jsonString = JSON.stringify(value, null, spaces).replace(/</g, '\\u003c')
+    return nunjucks.runtime.markSafe(jsonString)
+})
 
 module.exports = nunjucks;
