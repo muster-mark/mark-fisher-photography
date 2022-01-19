0c3a72e32646307f9a3ec3463fc02908543b5a90
Mark Fisher <mark.j.fisher@protonmail.com>
2022-01-19 17:57:31 +0000

    Add post-css plugin postcss-clamp

--- postcss.config.js
+++ postcss.config.js
@@ -3,6 +3,7 @@ module.exports = {
         require("autoprefixer"),
         require("postcss-logical")({
             dir: "ltr",
-        })
+        }),
+        require("postcss-clamp")(), // Clamp not supported in and_qq, and_uc, ios_saf < 13.4, kaios
     ]
 }
