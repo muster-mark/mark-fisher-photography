2f912e01a8815943e27fc3f833688ee02267e628
Mark Fisher <mark.j.fisher@protonmail.com>
2022-10-11 22:58:22 +0100

    Use next generation images

--- source/assets/js/components/explore-result.vue
+++ source/assets/js/components/explore-result.vue
@@ -1,10 +1,14 @@
 <template>
     <a :href="imageUrl">
-        <img width="200" :height="Math.round(200 / image.ImageAspectRatio)" :alt="image.Headline"
-             :srcset="'/photos/w200/' + image.Slug + '@2x.jpg 2x, /photos/w200/' + image.Slug + '.jpg 1x'"
-             :src="'/photos/w200/' + image.Slug + '.jpg'"
-             :style="{backgroundColor: image.Colors[0]}"
-        />
+        <picture>
+            <source type="image/jxl" :srcset="`/photos/w200/${image.Slug}@2x.jxl 2x, /photos/w200/${image.Slug}.jxl 1x`" />
+            <source type="image/webp" :srcset="`/photos/w200/${image.Slug}@2x.webp 2x, /photos/w200/${image.Slug}.webp 1x`" />
+            <img width="200" :height="Math.round(200 / image.ImageAspectRatio)" :alt="image.Headline"
+                 :srcset="`/photos/w200/${image.Slug}@2x.jpg 2x, /photos/w200/${image.Slug}.jpg 1x`"
+                 :src="'/photos/w200/' + image.Slug + '.jpg'"
+                 :style="{backgroundColor: image.Colors[0]}"
+            />
+        </picture>
     </a>
 </template>
 
