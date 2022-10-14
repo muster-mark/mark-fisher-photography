1ce1692a1d61273e79a2639f266dd6e8c7493ca3
Mark Fisher <mark.j.fisher@protonmail.com>
2022-10-14 21:32:07 +0100

    Install @jest/globals explicitly

--- local_modules/galleries.test.js
+++ local_modules/galleries.test.js
@@ -1,5 +1,9 @@
 const { galleries } = require("./galleries.js");
+const { expect } = require("@jest/globals")
 
-test("Meta descriptions all end in full stop", () => expect(galleries
-    .map(gallery => gallery.metaDescription[gallery.metaDescription.length - 1])
-    .every(last => [".", "?", "!"].indexOf(last) !== -1)).toBeTruthy());
+test("Meta descriptions all end in full stop", () => {
+    const doAllGalleryDescriptionsEndInPeriod = galleries
+        .map(gallery => gallery.metaDescription[gallery.metaDescription.length - 1])
+        .every(last => [".", "?", "!"].indexOf(last) !== -1);
+    expect(doAllGalleryDescriptionsEndInPeriod).toBeTruthy();
+});
