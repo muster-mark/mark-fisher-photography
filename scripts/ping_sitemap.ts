9e36fb69b250f21c904b609b15f91575effea5cd
Mark Fisher <mark.j.fisher@protonmail.com>
2023-04-23 20:43:33 +0100

    Use Promise.allSettled for sitemap pinging

--- scripts/ping_sitemap.ts
+++ scripts/ping_sitemap.ts
@@ -1,10 +1,11 @@
 import https from "node:https";
 import dotenv from "dotenv";
-dotenv.config({ path: `${__dirname}/../.production.env` });
+
+dotenv.config({path: `${__dirname}/../.production.env`});
 
 const sitemapUrl = `https://${process.env.URL}/sitemap.xml`;
 
-const ping = function(url: string): Promise<void> {
+const ping = function (url: string): Promise<void> {
     return new Promise((resolve, reject) => {
         const options = new URL(url);
         const req = https.request(options, (res) => {
@@ -19,18 +20,21 @@ const ping = function(url: string): Promise<void> {
 }
 
 async function main() {
-    const googleUrl = `https://www.google.com/webmasters/sitemaps/ping?sitemap=${sitemapUrl}`;
-    const bingUrl = `https://bing.com/webmaster/ping.aspx?sitemap=${sitemapUrl}`;
-
-    console.log(`Pinging ${googleUrl}`);
-    console.log(`Pinging ${bingUrl}`);
-
-    try {
-        await Promise.all([ping(googleUrl), ping(bingUrl)]);
-        console.log("Success");
-    } catch (err) {
-        console.error("Failed to ping search engines: ", err);
-    }
+    const urlsToPing = [
+        `https://www.google.com/webmasters/sitemaps/ping?sitemap=${sitemapUrl}`,
+        `https://bing.com/webmaster/ping.aspx?sitemap=${sitemapUrl}`, // TODO use IndexNow instead
+    ];
+    Promise.allSettled(urlsToPing.map(url => ping(url)))
+            .then(results => {
+                for (let i = 0; i < urlsToPing.length; i++) {
+                    const result = results[i];
+                    if (result.status === "fulfilled") {
+                        console.log(`Successfully pinged ${urlsToPing[i]}`);
+                    } else {
+                        console.error(`Failed to ping ${urlsToPing[i]}.`, result.reason);
+                    }
+                }
+            });
 }
 
 
