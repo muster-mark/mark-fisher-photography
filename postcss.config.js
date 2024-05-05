ff3ec61fed6089b0429bf966210d6680b88734f7
Mark Fisher <mark.j.fisher@protonmail.com>
2024-05-05 11:17:01 +0100

    Uninstall postcss-clamp. Good support, not even using.

--- postcss.config.js
+++ postcss.config.js
@@ -4,6 +4,5 @@ module.exports = {
         require("postcss-logical")({
             dir: "ltr",
         }),
-        require("postcss-clamp")(), // Clamp not supported in and_qq, and_uc, ios_saf < 13.4, kaios
     ]
 }
