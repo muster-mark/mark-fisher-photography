ca8f6f57ccbb2127f945ac32f6cf9490e7ea4f33
Mark Fisher <mark.j.fisher@protonmail.com>
2024-05-05 09:40:52 +0100

    port not set in env variable

--- scripts/serve.ts
+++ scripts/serve.ts
@@ -1,10 +1,12 @@
-import express from "express";
 import fs from "node:fs";
-import morgan from "morgan";
+
 import compression from "compression";
+import express from "express";
+import morgan from "morgan";
+
 import defaultHeaders from "./server/default_headers";
 
-const PORT = process.env.PORT || 8888;
+const port = 8888;
 
 const app = express();
 
@@ -15,7 +17,7 @@ app.use(morgan("dev"));
 app.use(compression());
 
 // Mock response for form submissions
-app.post("/message", (req, res) => {
+app.post("/message", (_req, res) => {
     // The response body is unimportant. Only the response code is relevant.
     res.send(JSON.stringify({ successMsg: "Your email has been successfully sent." }));
 });
@@ -25,30 +27,30 @@ app.use(express.static("public", {
     index: "index",
     setHeaders(res, path) {
         // Just need to set Content-Type for requests without an extension
-        if (path.split("/").pop().indexOf(".") === -1) {
+        if (path.split("/")?.pop()?.indexOf(".") === -1) {
             // Set default headers for documents only
             res.set("Content-Type", "text/html; charset=utf-8");
-            defaultHeaders.document.forEach((header: {name: string, value: string}) => {
+            defaultHeaders.document.forEach(header => {
                 res.set(header.name, header.value);
             });
         }
 
         // Set default headers
-        defaultHeaders.all.forEach((header: {name: string, value: string}) => {
+        defaultHeaders.all.forEach(header => {
             res.set(header.name, header.value);
         });
     },
 }));
 
 // static middleware only calls next() if it couldn't find the file
-app.use((req, res) => {
+app.use((_req, res) => {
     res.status(404);
     fs.createReadStream("public/404").pipe(res);
 });
 
 // Start server
-app.listen(PORT, () => {
-    console.log(`Server is listening on ${PORT}`);
+app.listen(port, () => {
+    console.log(`Server is listening on ${port}`);
 });
 
-console.log(`Website is available at http://localhost:${PORT}`);
+console.log(`Website is available at http://localhost:${port}`);
