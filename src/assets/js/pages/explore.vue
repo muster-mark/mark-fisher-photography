3cd13d5e9489f6ef6663a1ee78d195eed0926ca3
Mark Fisher <mark.j.fisher@protonmail.com>
2024-09-21 17:27:30 +0100

    Format file

--- src/assets/js/pages/explore.vue
+++ src/assets/js/pages/explore.vue
@@ -3,67 +3,62 @@
         <summary>
             <SvgIcon name="filter" />
             <span class="explore-filter_details-closed">Show filters</span>
-            <span class="explore-filter_details-open">Hide filters</span></summary>
+            <span class="explore-filter_details-open">Hide filters</span>
+        </summary>
         <div class="explore-filter_title">Seasons:</div>
         <div class="checkbox-grid">
-                <span v-for="seasonCount in seasonCounts" :key="`season-checkbox_${seasonCount.name}`">
-                    <input
-                            type="checkbox"
-                            :value="seasonCount.name"
-                            v-model="selectedSeasons"
-                            :id="`season-checkbox_${seasonCount.name}`"
-                            class="explore-filter-checkbox"
-                    />
-                    <label
-                            class="explore-filter-label"
-                            :for="`season-checkbox_${seasonCount.name}`"
-                    >
-                        {{ seasonCount.name[0].toUpperCase() + seasonCount.name.slice(1) }}&nbsp;({{
-                            seasonCount.count
-                        }})
-                    </label>
-                </span>
+            <span v-for="seasonCount in seasonCounts" :key="`season-checkbox_${seasonCount.name}`">
+                <input
+                    type="checkbox"
+                    :value="seasonCount.name"
+                    v-model="selectedSeasons"
+                    :id="`season-checkbox_${seasonCount.name}`"
+                    class="explore-filter-checkbox"
+                />
+                <label class="explore-filter-label" :for="`season-checkbox_${seasonCount.name}`">
+                    {{ seasonCount.name[0].toUpperCase() + seasonCount.name.slice(1) }}&nbsp;({{ seasonCount.count }})
+                </label>
+            </span>
         </div>
 
         <div class="explore-filter_title">Countries:</div>
         <div class="checkbox-grid">
-                <span v-for="countryCount in countryCounts" :key="`country-checkbox_${countryCount.name}`">
-                    <input
-                            type="checkbox"
-                            :value="countryCount.name"
-                            v-model="selectedCountries"
-                            :id="`country-checkbox_${countryCount.name}`"
-                            class="explore-filter-checkbox"
-                    />
-                    <label
-                            :for="`country-checkbox_${countryCount.name}`"
-                            class="explore-filter-label"
-                    >
-                        {{ countryCount.name.replace(" ", " ") }}&nbsp;({{ countryCount.count }})
-                    </label>
-                </span>
+            <span v-for="countryCount in countryCounts" :key="`country-checkbox_${countryCount.name}`">
+                <input
+                    type="checkbox"
+                    :value="countryCount.name"
+                    v-model="selectedCountries"
+                    :id="`country-checkbox_${countryCount.name}`"
+                    class="explore-filter-checkbox"
+                />
+                <label :for="`country-checkbox_${countryCount.name}`" class="explore-filter-label">
+                    {{ countryCount.name.replace(" ", " ") }}&nbsp;({{ countryCount.count }})
+                </label>
+            </span>
         </div>
     </details>
 
     <div ref="scrollTarget" class="explore_result-summary">
         <span v-if="!filteredImages.length">No images match your search criteria</span>
-        <span class="smaller" v-else>Showing <span class="larger">{{ firstShown }}-{{ lastShown }}</span> of <span
-                class="larger">{{ filteredImages.length }}</span> matching images</span>
+        <span class="smaller" v-else
+            >Showing <span class="larger">{{ firstShown }}-{{ lastShown }}</span> of
+            <span class="larger">{{ filteredImages.length }}</span> matching images</span
+        >
     </div>
 
     <div ref="masonryLayoutContainer">
         <masonry-layout
-                ref="masonryElement"
-                class="explore-results"
-                :cols="numColumns"
-                :gap="masonryGap"
-                :maxcolwidth="columnWidth"
-                :style="`width: ${masonryWidth}px`"
+            ref="masonryElement"
+            class="explore-results"
+            :cols="numColumns"
+            :gap="masonryGap"
+            :maxcolwidth="columnWidth"
+            :style="`width: ${masonryWidth}px`"
         >
             <div
-                    class="explore_result"
-                    v-for="image in filteredImages.slice(firstShown - 1, lastShown)"
-                    :key="image.Slug"
+                class="explore_result"
+                v-for="image in filteredImages.slice(firstShown - 1, lastShown)"
+                :key="image.Slug"
             >
                 <explore-result :image="image"></explore-result>
             </div>
@@ -71,29 +66,23 @@
     </div>
 
     <PaginationLinks
-            :page="page"
-            :numPages="numPages"
-            aria-controls=""
-            @page-change="goToPage($event)"
+        :page="page"
+        :numPages="numPages"
+        aria-controls=""
+        @page-change="goToPage($event)"
     ></PaginationLinks>
 </template>
 
 <script lang="ts">
 import "@appnest/masonry-layout";
-//import { MasonryLayout } from "@appnest/masonry-layout";
 </script>
 
 <script lang="ts" setup>
-import {computed, onMounted, onUnmounted, ref, useTemplateRef, watch} from "vue";
+import { computed, onMounted, onUnmounted, ref, useTemplateRef, watch } from "vue";
 import ExploreResult from "../components/explore-result.vue";
 import PaginationLinks from "../components/pagination-links.vue";
 import SvgIcon from "../components/svg-icon.vue";
-import {
-    type Image,
-    type CountryCount,
-    type SeasonCount,
-    Season,
-} from "../../../types";
+import { type Image, type CountryCount, type SeasonCount, Season } from "../../../types";
 
 const seasonCounts = ref<SeasonCount[]>([]);
 const selectedSeasons = ref<Season[]>([Season.spring, Season.summer, Season.autumn, Season.winter]);
@@ -110,7 +99,7 @@ const masonryLayoutContainer = useTemplateRef("masonryLayoutContainer");
 const masonryElement = useTemplateRef("masonryElement");
 
 const filteredImages = computed(() => {
-    return allImages.value.filter(image => matchesCountry(image) && matchesSeason(image));
+    return allImages.value.filter((image) => matchesCountry(image) && matchesSeason(image));
 });
 
 const firstShown = computed(() => {
@@ -118,7 +107,7 @@ const firstShown = computed(() => {
 });
 
 const lastShown = computed(() => {
-    return Math.min(filteredImages.value.length, resultLimit * (page.value));
+    return Math.min(filteredImages.value.length, resultLimit * page.value);
 });
 
 const numPages = computed(() => {
@@ -151,23 +140,25 @@ function goToPage(pageNumber: number) {
 }
 
 function updateColumns() {
-    numColumns.value = Math.floor((masonryLayoutContainer.value.clientWidth + masonryGap.value) / (columnWidth + masonryGap.value));
+    numColumns.value = Math.floor(
+        (masonryLayoutContainer.value.clientWidth + masonryGap.value) / (columnWidth + masonryGap.value),
+    );
     masonryElement.value.setAttribute("gap", `${masonryGap.value}`); // Seems to be bug in @appnest component
 }
 
 // This is pre-loaded on this page
 fetch("/data/images.json")
-        .then(data => data.json())
-        .then(json => {
-            allImages.value = json.images;
-            countryCounts.value = json.countryCounts;
-            selectedCountries.value = json.countryCounts.map((countryCount: CountryCount) => countryCount.name);
-            selectedCountries.value = Array.from(new Set(selectedCountries.value));
-            seasonCounts.value = json.seasonCounts;
-        })
-        .catch(err => {
-            console.error("There was an error fetching data", err);
-        });
+    .then((data) => data.json())
+    .then((json) => {
+        allImages.value = json.images;
+        countryCounts.value = json.countryCounts;
+        selectedCountries.value = json.countryCounts.map((countryCount: CountryCount) => countryCount.name);
+        selectedCountries.value = Array.from(new Set(selectedCountries.value));
+        seasonCounts.value = json.seasonCounts;
+    })
+    .catch((err) => {
+        console.error("There was an error fetching data", err);
+    });
 
 onMounted(() => {
     masonryElement.value.setAttribute("gap", `${masonryGap.value}`); // Seems to be bug in @appnest component
@@ -178,21 +169,20 @@ onMounted(() => {
         resizeObserver.observe(masonryLayoutContainer.value);
     } else {
         // Fallback for Safari < 13.1
-        (["resize", "orientationchange"] as const).forEach(event => {
-            window.addEventListener(event, updateColumns, {passive: true})
+        (["resize", "orientationchange"] as const).forEach((event) => {
+            window.addEventListener(event, updateColumns, { passive: true });
         });
     }
 });
 
 onUnmounted(() => {
-    (["resize", "orientationchange"] as const).forEach(event => {
+    (["resize", "orientationchange"] as const).forEach((event) => {
         window.removeEventListener(event, updateColumns);
     });
 });
 </script>
 
 <style lang="scss">
-
 // Scoped styles not working here because of https://github.com/vuejs/vue-loader/issues/1915
 
 .explore-results {
@@ -245,7 +235,7 @@ onUnmounted(() => {
     }
 
     summary .ico {
-        vertical-align: -.1em;
+        vertical-align: -0.1em;
     }
 
     &_title {
@@ -277,40 +267,41 @@ $checkbox-size: 20px;
     margin-right: 1em;
     transition: clip-path 0.25s;
     clip-path: polygon(
-                    0% 0%,
-                    100% 0%,
-                    100% 100%,
-                    0% 100%,
-                    0% 52.3%,
-                    10% 10%,
-                    10% 90%,
-                    90% 90%,
-                    90% 10%,
-                    41% 10%,
-                    10% 10%,
-                    10% 52.3%,
-                    0% 52.3%,
-                    0% 0%);
+        0% 0%,
+        100% 0%,
+        100% 100%,
+        0% 100%,
+        0% 52.3%,
+        10% 10%,
+        10% 90%,
+        90% 90%,
+        90% 10%,
+        41% 10%,
+        10% 10%,
+        10% 52.3%,
+        0% 52.3%,
+        0% 0%
+    );
 }
 
 .explore-filter-checkbox:checked + label::before {
     clip-path: polygon(
-                    0% 0%,
-                    100% 0%,
-                    100% 100%,
-                    0% 100%,
-                    0% 52.3%,
-                    14.1% 52.3%,
-                    41% 79.7%,
-                    86.6% 33.5%,
-                    76.3% 25.6%,
-                    41% 60.5%,
-                    23.2% 43.2%,
-                    14.1% 52.3%,
-                    0% 52.3%,
-                    0% 0%);
+        0% 0%,
+        100% 0%,
+        100% 100%,
+        0% 100%,
+        0% 52.3%,
+        14.1% 52.3%,
+        41% 79.7%,
+        86.6% 33.5%,
+        76.3% 25.6%,
+        41% 60.5%,
+        23.2% 43.2%,
+        14.1% 52.3%,
+        0% 52.3%,
+        0% 0%
+    );
 }
-
 
 .explore-filter-label {
     display: inline-grid;
@@ -329,5 +320,4 @@ $checkbox-size: 20px;
         height: $checkbox-size;
     }
 }
-
 </style>
