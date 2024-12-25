99d7f8efba302142268e2a89d4089b79519d2f0f
Mark Fisher <mark.j.fisher@protonmail.com>
2024-12-25 09:05:06 +0000

    Add console surprise

--- src/assets/js/main.ts
+++ src/assets/js/main.ts
@@ -1,4 +1,4 @@
-import {createApp} from "vue";
+import { createApp } from "vue";
 
 import explore from "./pages/explore.vue";
 import contact from "./pages/contact.vue";
@@ -9,11 +9,58 @@ require("../css/main.scss");
 //@ts-expect-error
 __webpack_public_path__ = "assets/";
 
-if (document.getElementById("explore-app")) {
-    createApp(explore)
-            .mount(document.getElementById("explore-app"));
+const appElement = document.getElementById("app");
+
+if (appElement) {
+    createApp(explore).mount(appElement);
 }
 
-if (document.getElementById("contact-app")) {
-    createApp(contact).mount(document.getElementById("contact-app"));
+const contactElement = document.getElementById("contact-app");
+if (contactElement) {
+    createApp(contact).mount(contactElement);
+}
+
+async function consoleSurprise() {
+    const isFirefox = /firefox/i.test(navigator.userAgent);
+    const { color, backgroundColor } = getComputedStyle(document.body);
+    const height = 60;
+
+    const svgText = await fetch("/favicon.svg", { priority: "low" }).then((response) => response.text());
+    const svg = new DOMParser().parseFromString(svgText, "image/svg+xml").documentElement;
+    svg.setAttribute("width", `${height}`);
+    svg.setAttribute("height", `${height}`);
+
+    const style = document.createElement("div").style;
+    style.fontSize = "1.5em";
+    style.backgroundColor = backgroundColor;
+    style.color = color;
+    style.lineHeight = `${height}px`;
+    style.paddingBlock = isFirefox ? `${height / 2}px` : "0";
+    style.paddingLeft = "70px";
+    style.paddingRight = "10px";
+    style.backgroundRepeat = "no-repeat";
+    style.backgroundPositionX = "10px";
+    style.backgroundPositionY = isFirefox ? "10px" : "0";
+    style.backgroundImage = `url("data:image/svg+xml,${encodeURIComponent(svg.outerHTML)}")`;
+
+    console.info("%c Look at you poking around in DevTools.", style.cssText);
+
+    const height2 = 44;
+    style.cssText = "";
+    style.backgroundColor = backgroundColor;
+    style.color = color;
+    style.paddingInline = "1em";
+    style.paddingBlock = isFirefox ? `${height2 / 2}px` : "0";
+    style.lineHeight = `${height2}px`;
+    style.fontSize = "1.25em";
+    console.info(
+        "%c Check out the code, if you want, at https://github.com/muster-mark/mark-fisher-photography",
+        style.cssText,
+    );
+}
+
+if ("requestIdleCallback" in window) {
+    requestIdleCallback(consoleSurprise);
+} else {
+    setTimeout(consoleSurprise, 1000);
 }
