149e7d327be1215149eda68ce1bfe8186e1bdd40
Mark Fisher <mark.j.fisher@protonmail.com>
2025-03-10 21:41:26 +0000

    Remove get-pixels dependency (has dependency on request, which has vulnerability)

--- local_modules/get_image_colors.ts
+++ local_modules/get_image_colors.ts
@@ -1,46 +1,48 @@
 // From https://github.com/colorjs/get-image-colors
 // Without the svg stuff, due to vulnerabilities in get-svg-colors
 
-import getPixels from "get-pixels";
+import fs from "fs/promises";
+
+import chroma from "chroma-js";
 //@ts-ignore
 import getRgbaPalette from "get-rgba-palette";
-import chroma from "chroma-js";
+import jpeg from "jpeg-js";
+import ndarray from "ndarray";
 
 interface Options {
-    type?: string;
     count?: number;
 }
 
-function colorPalette(input: string, options: Options): Promise<chroma.Color[]> {
-    options = options ?? {
-        type: undefined,
+async function getPixels(path: string) {
+    const data = await fs.readFile(path);
+    const jpegData = jpeg.decode(data);
+    if (!jpegData) {
+      throw new Error("Error decoding jpeg")
+    }
+    var nshape = [jpegData.height, jpegData.width, 4];
+    var result = ndarray(jpegData.data, nshape);
+    return result.transpose(1, 0);
+}
+
+async function paletteFromBitmap(filename: string, options: Options): Promise<chroma.Color[]> {
+    options = {
         count: 5,
+        ...options,
     };
 
-    return new Promise((resolve, reject) => {
-        paletteFromBitmap(input, options).then(resolve).catch(reject);
+    const pixels = await getPixels(filename);
+    return getRgbaPalette(pixels.data, options.count).map(function (rgba: [number, number, number]) {
+        return chroma(rgba);
     });
 }
 
-function paletteFromBitmap(filename: string, options: Options): Promise<chroma.Color[]> {
-    options = options ?? {
-        type: undefined,
+async function colorPalette(input: string, options: Options): Promise<chroma.Color[]> {
+    options = {
         count: 5,
+        ...options,
     };
 
-    return new Promise((resolve, reject) => {
-        getPixels(filename, options.type, (err, pixels) => {
-            if (err) {
-                reject(err);
-            }
-
-            const palette = getRgbaPalette(pixels.data, options.count).map(function (rgba: [number, number, number]) {
-                return chroma(rgba);
-            });
-
-            resolve(palette);
-        });
-    });
+    return paletteFromBitmap(input, options);
 }
 
 export default colorPalette;
