f1791ca8e5b2c09bc211a98322de77de27262f73
Mark Fisher <mark.j.fisher@protonmail.com>
2025-06-19 17:23:02 +0100

    Fix deprecation in jest config

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,9 @@
-const { defaults: tsjPreset } = require("ts-jest/presets");
-
 module.exports = {
-    globals: {
-        "ts-jest": {
-            tsconfig: "<rootDir>/tsconfig.json",
-        },
-    },
     moduleFileExtensions: ["js", "json", "ts"],
     testEnvironment: "node",
     transform: {
         "^.+\\.js$": "babel-jest",
-        ...tsjPreset.transform,
+        "^.+\\.ts$": ["ts-jest", { tsconfig: "<rootDir>/tsconfig.json" }],
     },
     testPathIgnorePatterns: ["<rootDir>/e2e/"],
 };
