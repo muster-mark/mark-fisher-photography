295e2099e16348381f73d6b0c5d955519be598f4
Mark Fisher <mark.j.fisher@protonmail.com>
2026-01-23 18:32:25 +0000

    Remove postcss-logical. Widely supported now (from Safari 15)

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,6 @@
 module.exports = {
     plugins: [
         require("autoprefixer"),
-        require("postcss-logical")({
-            dir: "ltr",
-        }),
         require("cssnano")({
             preset: "default",
         }),
